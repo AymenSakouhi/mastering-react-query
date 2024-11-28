ea89a529a305c7e3fd1e583dc82a149f6013d088
Aymen Sakouhi <aymenmarketer@gmail.com>
2024-11-28 02:44:36 +0100

    how infinite queries work

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,56 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { Person } from './Person'
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/'
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
-};
+  const response = await fetch(url)
+  return response.json()
+}
 
 export function InfinitePeople() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    error,
+    isError
+  } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    // page params is basically the api fetch url.
+    queryFn: ({ pageParams = initialUrl }) => fetchUrl(pageParams),
+    // hasNext page is going to be either undefiend or the nextpage url for fetching
+    getNextPageParam: (lastPage) => lastPage.next || undefined
+  })
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  if (isLoading) return <h2 className="loading">Loading....!</h2>
+  if (isError) return <h2>{error.toString()}</h2>
+
+  return (
+    <>
+      {isFetching && <h2 className="loading">Loading....!</h2>}
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) fetchNextPage()
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            )
+          })
+        })}
+      </InfiniteScroll>
+    </>
+  )
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,55 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { Species } from './Species'
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/'
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
-};
+  const response = await fetch(url)
+  return response.json()
+}
 
 export function InfiniteSpecies() {
+  const {
+    data,
+    // hasNextPage boolean to undicate pageParam content truthy or falsy
+    hasNextPage,
+    fetchNextPage,
+    isFetching,
+    isError,
+    error,
+    isLoading
+  } = useInfiniteQuery({
+    queryKey: ['sw-species'],
+    queryFn: ({ pageParams = initialUrl }) => fetchUrl(pageParams),
+    // you can get the pageParams from lastPage or allPages depending on api
+    getNextPageParam: (lastPage) => lastPage.next || undefined
+  })
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  if (isLoading) return <div className="loading">Loading...!</div>
+  if (isError) return <div>{error.toString()}</div>
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...!</div>}
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) fetchNextPage()
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) =>
+          pageData.results.map((specie) => (
+            <Species
+              {...{
+                key: specie.name,
+                name: specie.name,
+                language: specie.language,
+                averageLifespan: specie.average_lifespan
+              }}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
+  )
 }
